75a30dc5366fe194c43b932e36b52406ac9ebf8f
montes8 <emontesinos812@gmail.com>
2018-10-08 18:30:55 -0500

    creando base de datos

--- app/src/main/java/com/example/tayler_gabbi/demomvp_java/database/DataBase.java
+++ app/src/main/java/com/example/tayler_gabbi/demomvp_java/database/DataBase.java
@@ -1,12 +1,13 @@
 package com.example.tayler_gabbi.demomvp_java.database;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
 
 import com.example.tayler_gabbi.demomvp_java.database.dao.UsuarioDao;
 import com.example.tayler_gabbi.demomvp_java.model.Usuario;
 
 @Database(entities = { Usuario.class},version = 1)
-public abstract class DataBase {
+public abstract class DataBase extends RoomDatabase{
 
     public abstract UsuarioDao usuarioDao();
 }
